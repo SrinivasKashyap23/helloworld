af96c92f988edc16ad5c336445fe717d2cdb1341
SrinivasKashyap23 <06kashyapch@gmail.com>
2024-10-22 16:18:50 +0530

    Initial commit

---
+++ file1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+dataset = pd.read_csv(r'C:\Users\hp\OneDrive\Desktop\PROJECT SCHOOL\archive\fitness_tracker_dataset.csv')
+print(dataset.head(20))
+dataset.fillna(dataset.mean(), inplace=True)
+input_data = dataset[['steps', 'distance_km', 'workout_type']]
+
+
+
